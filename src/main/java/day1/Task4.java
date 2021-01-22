36a0c06c094f95e524f7e185a0d9270cf0641447
Sergey Ovsyannikov <sergalin@gmail.com>
2021-01-22 13:24:10 +0300

    Change loop iteration

--- src/main/java/day1/Task4.java
+++ src/main/java/day1/Task4.java
@@ -5,7 +5,7 @@ public class Task4 {
     	int year = 1980;
     	while (year <= 2020) {
     		System.out.println("Олимпиада " + year + " года");
-    		year = year + 4;
+    		year += 4;
     	}
 
     }
