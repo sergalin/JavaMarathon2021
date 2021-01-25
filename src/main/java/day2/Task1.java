2faab9b702ebdecea3fd0020297d8bf8b770b042
Sergey Ovsyannikov <sergalin@gmail.com>
2021-01-25 16:31:20 +0300

    complete day2 tasks

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,19 @@
 package day2;
-
+import java.util.Scanner;
 public class Task1 {
     public static void main(String[] args) {
+        Scanner readInput = new Scanner(System.in);
+        int floor = readInput.nextInt();
+
+        if (floor >= 1 && floor <= 4) {
+            System.out.println("Малоэтажный дом");
+        } else if (floor >= 5 && floor <= 8) {
+            System.out.println("Среднеэтажный дом");
+        } else if (floor >= 9) {
+            System.out.println("Многоэтажный дом");
+        } else {
+            System.out.println("Ошибка ввода");
+        }
 
     }
 }
