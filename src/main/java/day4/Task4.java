8dbf40ce83b7bc1edf85b4fb8d2c844501532d2b
Sergey Ovsyannikov <sergalin@gmail.com>
2021-01-27 14:03:04 +0300

    complete day4 tasks (100 elements)

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class Task4 {
     public static void main(String[] args) {
-        int[] myArray = new int[11];
+        int[] myArray = new int[100];
         Random random = new Random();
 
         for (int i = 0; i < myArray.length; i++) {
