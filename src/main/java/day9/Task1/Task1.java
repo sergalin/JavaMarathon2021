61b5982890d90ab8df0161dbac688c7c40772211
Sergey Ovsyannikov <sergalin@gmail.com>
2021-01-29 11:42:11 +0300

    complete day9 tasks

--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,12 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Ivan", "23");
+        Teacher teacher = new Teacher("Vasiliy", "Music");
+        student.printInfo();
+        System.out.println(student.getGroupName());
+        teacher.printInfo();
+        System.out.println(teacher.getSubjectName());
 
     }
 }
---
+++ src/main/java/day9/Task2/Rectangle.java
@@ -0,0 +1,30 @@
+package day9.Task2;
+
+public class Rectangle extends Figure{
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height, String color) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    @Override
+    public double area() {
+        return width * height;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+}
